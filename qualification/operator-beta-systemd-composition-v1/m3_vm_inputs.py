a72db00fc8d139a2ea045de69c328ac0d74f07c8
James Beck <unpingable@users.noreply.github.com>
2026-09-09 04:26:09 -0400

    Bind controller-loss VM to independently verified package003

--- qualification/operator-beta-systemd-composition-v1/m3_vm_inputs.py
+++ qualification/operator-beta-systemd-composition-v1/m3_vm_inputs.py
@@ -9,8 +9,8 @@ NQ_BINARY_SHA = '370c4fdff391886460a56747c9b894ba4bd53a2f37b796bfb7f7966d1229bd8
 PINS = {
     'nq_package': 'bb9b89fbe87d2b9b720de497c8a8f96e00aabfeadb0a7598fe0acc8c4fed76ca',
     'nq_receipt': '601304c5f27d525d1da0ccde08be73ca54d72fac1122afad89013c651dcd27d8',
-    'driver_package': '8b2e780935c999a71c15b774b19f61a50ae4bdf88c0aedd67d1d7e6b265f4f83',
-    'driver_receipt': 'c7108ac52fe5cfb16bd778f8ee6f43d654bdca87a3634daac24d1812d89635f1',
+    'driver_package': 'c6a6a09aa801acbba820a4dafab96abf6fa3385e5d449e5e855244286f41cf3d',
+    'driver_receipt': '6fb62522e4bf3e9ee90600988f15d54a86f73abc89b0ae513d23d0fea9eda619',
     'composition_package': '54ac28c11c1b7cb54f621217c786086d481a4ccbae41a4bc15546995471f9bd5',
     'composition_receipt': '0d02551ae4666e129f759a6e643037f2fb514aaef7b0c78850f17b03714b050b',
     'websockets': '6abbd3e82c731c8e531714466acd5d87b5e88ac3243465337ba71d68e23ae7e3',
